39f11921a8b26ea243eb3cf71e81c5b28e6e2897
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2023-01-18 17:09:34 +0100

    komentarze

--- py-scope-control.py
+++ py-scope-control.py
@@ -23,27 +23,29 @@ def init(ip):
 
 def main():
     key.add_hotkey('q', lambda: inst.write("AUToscale"))
+#załączanie kanałów
     key.add_hotkey('1', lambda: chan(1,1))
     key.add_hotkey('2', lambda: chan(2,1))
     key.add_hotkey('3', lambda: chan(3,1))
     key.add_hotkey('4', lambda: chan(4,1))
-    key.add_hotkey('ctrl + 1', lambda: chan(1,0))
-    key.add_hotkey('ctrl + 2', lambda: chan(2,0))
-    key.add_hotkey('ctrl + 3', lambda: chan(3,0))
-    key.add_hotkey('ctrl + 4', lambda: chan(4,0))
-    # key.add_hotkey('F1', lambda: chan(1))
-    # key.add_hotkey('F2', lambda: chan(2))
-    # key.add_hotkey('F3', lambda: chan(3))
-    # key.add_hotkey('F4', lambda: chan(4))
+#wybór kanału
+    key.add_hotkey('F1', lambda: chan(1,0))
+    key.add_hotkey('F2', lambda: chan(2,0))
+    key.add_hotkey('F3', lambda: chan(3,0))
+    key.add_hotkey('F4', lambda: chan(4,0))
+
     key.add_hotkey('b', lambda: inst.write(":BEEP"))
+#wzmocnienie
     key.add_hotkey('w', lambda: ampVert('up'))
     key.add_hotkey('s', lambda: ampVert('down'))
     key.add_hotkey('a', lambda: ampHoriz('left'))
     key.add_hotkey('d', lambda: ampHoriz('right'))
-    key.add_hotkey('ctrl + w', lambda: ampVert('up'))
-    key.add_hotkey('ctrl + s', lambda: ampVert('down'))
-    key.add_hotkey('ctrl + a', lambda: ampHoriz('left'))
-    key.add_hotkey('ctrl + d', lambda: ampHoriz('right'))
+#offset
+    key.add_hotkey('ctrl + w', lambda: offVert('up'))
+    key.add_hotkey('ctrl + s', lambda: offVert('down'))
+    key.add_hotkey('ctrl + a', lambda: offHoriz('left'))
+    key.add_hotkey('ctrl + d', lambda: offHoriz('right'))
+    
     key.add_hotkey('esc', lambda: close())
     while 1:
         print("wybierz komende: \
@@ -53,29 +55,7 @@ def main():
         \nb - bip \
         \nw, a, s, d - wzmocnienie, podstawa czasu \
         \nctrl + w, a ,s, d - offset \
-        \nesc - przerwij połączenie")
-        try:
-            key.wait()
-        except:
-            print("złe polecenie")
-
-        # if k=="1":
-        #     inst.write(":AUToscale")
-        # elif k=="2":
-        #     chan()
-        # elif k=="3":
-        #     vert()
-        # elif k=="4":
-        #     horiz()
-        # elif k=="5":
-        #     inst.write(":BEEP")
-        # elif k=="6":
-        #     inst.close()
-        #     break
-        # else:
-        #     print("złe polecenie")
-        # print("\n\n\n")
-
+        \nesc - przerwij połączenie, zakończ program")
 
 def chan(ch: int, sw: int):
     ch = str(ch)
@@ -86,12 +66,6 @@ def chan(ch: int, sw: int):
 
 
 def ampVert(op: str):
-    # ch = input("wybierz kanał: ")
-    # ch = 1
-    # ch = str(ch)
-    # print(inst.query(":CHANnel" + ch + ":SCALe?"))
-    # scale = float(inst.query(":CHANnel" + ch + ":SCALe?"))
-    # #op = input("up - zwiększ \ndown - zmniejsz \n")
     print(inst.query(":CHANnel" + ch + ":SCALe?"))
     scale = float(inst.query(":CHANnel" + ch + ":SCALe?"))
     if op=="up":
@@ -106,14 +80,12 @@ def ampHoriz(op: str):
     print(inst.query(":TIMebase:RANGe?"))
     scale = float(inst.query(":TIMebase:RANGe?"))
     print(scale)
-    #op = input("up - zwiększ \ndown - zmniejsz \n")
     if op=="right":
         scale=str(scale/2)
     elif op=="left":
         scale=str(scale*2)
     else:
         print("złe polecenie")
-
     print(scale)
     inst.write(":TIMebase:RANGe " + scale)
 
@@ -138,7 +110,6 @@ def offHoriz(op: str):
         off=str(off-0.1*off)
     else:
         print("złe polecenie")
-
     print(off)
     inst.write(":TIMebase:WINDow:POSition" + off)
 

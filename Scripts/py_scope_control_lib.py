6c1c6a77c87e62c429b3d6fb6c86d4df33361c6b
rolf213 <121953201+rolf213@users.noreply.github.com>
2023-02-09 10:50:08 +0100

    Add files via upload

---
+++ Scripts/py_scope_control_lib.py
@@ -0,0 +1,68 @@
+#biblioteki: pyvisa   pyvisa-py
+
+def init(ip):
+    try:
+        global inst
+        global idn
+        inst = rm.open_resource("TCPIP0::192.168.0." + ip + "::INSTR")
+        idn = inst.query("*IDN?")
+        print("połączenie udane \n" + "IDN: \n" + idn + "\n\n")
+        return 1
+    except:
+        print("\npołączenie nieudane\n")
+        return 0
+
+def chan(ch: int):
+    ch = str(ch)
+    state = inst.query(":CHANnel" + ch + ":DISPlay?")
+    state = str(1-int(state))
+    inst.write(":CHANnel" + ch + ":DISPlay " + state)
+    print("test")
+
+def ampVert(op: str):
+    # ch = input("wybierz kanał: ")
+    # ch = 1
+    # ch = str(ch)
+    # print(inst.query(":CHANnel" + ch + ":SCALe?"))
+    # scale = float(inst.query(":CHANnel" + ch + ":SCALe?"))
+    # #op = input("up - zwiększ \ndown - zmniejsz \n")
+    print(inst.query(":CHANnel" + "1" + ":SCALe?"))
+    scale = float(inst.query(":CHANnel" + "1" + ":SCALe?"))
+    if op=="up":
+        scale=str(scale/1.5)
+    elif op=="down":
+        scale=str(scale*1.5)
+    else:
+        print("złe polecenie")
+    inst.write(":CHANnel" + "1" + ":SCALe " + scale)
+
+def ampHoriz(op: str):
+    print(inst.query(":TIMebase:RANGe?"))
+    scale = float(inst.query(":TIMebase:RANGe?"))
+    print(scale)
+    #op = input("up - zwiększ \ndown - zmniejsz \n")
+    if op=="right":
+        scale=str(scale/2)
+    elif op=="left":
+        scale=str(scale*2)
+    else:
+        print("złe polecenie")
+
+    print(scale)
+    inst.write(":TIMebase:RANGe " + scale)
+
+def close():
+    inst.close()
+    return
+
+
+
+
+
+
+    
+    
+    
+    
+
+
